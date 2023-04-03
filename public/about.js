929e88e836b37696a9d8e4742ea27507404559fa
mcghieb <mcghie.blake@gmail.com>
2023-04-03 17:26:47 -0600

    edit(*) added third party endpoint call

--- public/about.js
+++ public/about.js
@@ -4,3 +4,26 @@ const roomCodeText = localStorage.getItem('room-code');
         roomCode = JSON.parse(roomCodeText);
     }
 document.querySelector('.room-code').innerHTML = `<h5>#${roomCode}</h5>`;
+
+
+function displayQuote(data) {
+    fetch('https://api.quotable.io/random')
+      .then((response) => response.json())
+      .then((data) => {
+        const containerEl = document.querySelector('#quote');
+  
+        const quoteEl = document.createElement('p');
+        quoteEl.classList.add('quote');
+        const authorEl = document.createElement('p');
+        authorEl.classList.add('author');
+
+  
+        quoteEl.textContent = data.content;
+        authorEl.textContent = data.author;
+  
+        containerEl.appendChild(quoteEl);
+        containerEl.appendChild(authorEl);
+      });
+  }
+
+displayQuote()
