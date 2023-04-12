e72e07276ec8c4277ad578fdd1c0a7c81e31fe51
mcghieb <mcghie.blake@gmail.com>
2023-04-11 21:04:06 -0600

    edit(env) ready for components

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Hi</p>
+      </header>
+    </div>
+  );
+}
+
+function HeaderGeneric() {
+
+};
+
+
+
+
+export default App;
